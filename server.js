77bd69f95242fd28296c3a337ff339aa8516f8f4
Yozora <yozora391@outlook.com>
2026-07-20 23:58:14 +0300

    fix: correct database path resolution in server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const require = createRequire(import.meta.url);
 const app = require("./app").default;
 
 const sqlite3 = require("sqlite3").verbose();
-const dbPath = path.join(process.cwd(), "", "dbtest.db");
+const dbPath = path.join(process.cwd(), "dbtest.db");
 // eslint-disable-next-line no-unused-vars
 const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READONLY, (err) => {
   if (err) {
